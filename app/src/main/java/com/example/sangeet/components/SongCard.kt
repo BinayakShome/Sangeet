f2cbfbac961eb49889a1e5dfd3ee3b7baff39a9c
BinayakShome <binayakshome3@gmail.com>
2025-08-06 17:11:53 +0530

    minor changes

--- app/src/main/java/com/example/sangeet/components/SongCard.kt
+++ app/src/main/java/com/example/sangeet/components/SongCard.kt
@@ -1,38 +1,39 @@
 package com.example.sangeet.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.sangeet.R
 import com.example.sangeet.data.Song
 
 @Composable
 fun SongCard(song: Song, onClick: () -> Unit) {
+
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.cd_animation))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+
     Card(
         modifier = Modifier
             .padding(8.dp)
@@ -44,12 +45,11 @@ fun SongCard(song: Song, onClick: () -> Unit) {
             .padding(8.dp)
         ) {
             Row {
-            Image(
-                painter = painterResource(id = R.drawable.music_icon),
-                contentDescription = "Music Icon",
-                modifier = Modifier.fillMaxSize(.13f)
-                    .padding(top = 0.dp)// You can adjust the size
-            )
+                LottieAnimation(
+                    composition = composition,
+                    progress = progress,
+                    modifier = Modifier.size(40.dp)
+                )
                 Spacer(modifier = Modifier.width(18.dp))
             Text(
                 text = song.title,
